c813143abbffacb2ceb9b7fac9181e38bef28595
AlysQueZone <AlysQueZone@users.noreply.github.com>
2026-09-15 17:58:22 +0300

    Гамба: спин-звук belaya-noch

--- src/lib/gamba.ts
+++ src/lib/gamba.ts
@@ -47,6 +47,7 @@ export const GAMBA_SPIN_SOUNDS = [
   'gamba-spin-maknaggetskue.mp3',
   'gamba-spin-otvinta.mp3',
   'gamba-spin-rgg-koleso.mp3',
+  'gamba-spin-belaya-noch.mp3',
   'gamba-bg.mp3',
   'gamba-win-taktak.mp3',
 ] as const;
