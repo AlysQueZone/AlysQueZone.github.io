b935b37a4db8f1cac0c32898d1d8e274ccb0b1ca
AlysQueZone <AlysQueZone@users.noreply.github.com>
2026-09-15 17:48:28 +0300

    Гамба: спин-звук maknaggetskue

--- src/lib/gamba.ts
+++ src/lib/gamba.ts
@@ -44,6 +44,7 @@ export const GAMBA_SPIN_SOUNDS = [
   'gamba-spin-tutututu.mp3',
   'gamba-spin-kazu-tatata.mp3',
   'gamba-spin-tratatatata.mp3',
+  'gamba-spin-maknaggetskue.mp3',
   'gamba-bg.mp3',
   'gamba-win-taktak.mp3',
 ] as const;
